ab8c2ed20c3947930b499afb8ad98c6021dfb2d2
renyzeraa <renansilvaytb@gmail.com>
2022-08-16 20:40:10 -0300

    fix rename pasta

---
+++ exercices.pt2/Ex_06.java
@@ -0,0 +1,19 @@
+package Lista2;
+
+import java.util.Scanner;
+
+public class Ex_06 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int num = scan.nextInt();
+        if (num % 2 == 0) {
+            num++;
+        }
+        System.out.println(num);
+        scan.close();
+    }
+
+    }
---
+++ exercices.pt2/ex03.java
@@ -0,0 +1,29 @@
+package Lista2;
+
+import java.util.Scanner;
+
+public class Ex_03 {
+	
+	static Scanner entrada;
+
+	public static void main(String[] args) {
+		
+		entrada = new Scanner(System.in);
+		char letra;
+		boolean sair = true;
+		
+		while (sair) {
+		System.out.print("Digite M para masculino e F para feminino: ");
+		letra = entrada.next().charAt(0);
+		if(letra == 'M' || letra == 'm') {
+			System.out.println("masculino");
+			sair = false;
+		}else if(letra == 'f' || letra == 'F') {
+			System.out.println("feminino" );
+			sair = false;
+		}else 
+			System.out.println("informação errada, digite novamente");
+		}
+		entrada.close();
+	}
+}
